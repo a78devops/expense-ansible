c38561e863b000d90db519c14eb17bd8321253c8
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-06-22 13:44:10 +0530

    update secrets playbook

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-#ansible-playbook -i $component-$env.bdevops55.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.bdevops55.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
