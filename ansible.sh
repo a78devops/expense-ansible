276ccb48bc6ace991fc3172d02172b896f776458
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-09-04 11:32:18 +0530

    update secrets referring from one secret and enable RDS

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,5 @@
 component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-exit 1
 ansible-playbook -i $component-$env.bdevops55.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
 rm -f ~/*.json
